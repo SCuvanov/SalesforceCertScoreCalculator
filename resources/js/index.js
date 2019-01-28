d22aa2a4cf826d389ff6244d0ee7f5c118c0eacd
Sean Cuvanov <Sean.Cuvanov@icims.com>
2019-01-27 22:15:59 -0500

    Adding some detail to the dropdown menu

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,13 @@
 function hideShowDropdownMenu() {
     let dropdownMenu = document.getElementById('dropdown-menu');
+    let btnIcon = document.getElementById('btn-icon');
     if (dropdownMenu.classList.contains('hide')) {
         dropdownMenu.classList.remove('hide');
+        btnIcon.classList.remove('fa-angle-down');
+        btnIcon.classList.add('fa-angle-up');
     } else {
         dropdownMenu.classList.add('hide');
+        btnIcon.classList.remove('fa-angle-up');
+        btnIcon.classList.add('fa-angle-down');
     }
 }
